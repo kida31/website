75cb721378f44fba3fa6649e277929522b6b3f55
kida31 <83455653+kida31@users.noreply.github.com>
2024-09-05 21:37:51 +0200

    Add bg image to card

--- src/components/domain-card.tsx
+++ src/components/domain-card.tsx
@@ -1,13 +1,38 @@
+import { CSSProperties } from 'react';
+
 type DomainCardProps = {
     title: string;
     description?: string;
+    imgUrl?: string;
+}
+
+const parentContainer: CSSProperties = {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    padding: "1rem",
+}
+
+const textContainerStyle: CSSProperties = {
+    zIndex: 1,
+}
+
+const bgImgStyle: CSSProperties = {
+    position: "absolute",
 }
 
 export default function DomainCard(props: DomainCardProps) {
     return (
-        <div className="card">
-            <h2>{props.title}</h2>
-            {props.description ? <p>{props.description}</p> : null}
+        <div style={parentContainer} >
+            <div style={textContainerStyle}>
+                <h2>{props.title}</h2>
+                {props.description ? <p>{props.description}</p> : null}
+            </div>
+            <img src={props.imgUrl} style={bgImgStyle} />
         </div>
     )
 }
